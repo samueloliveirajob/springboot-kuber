873f860d475f1b0088f36dc4afe288d4ffbd9f1d
samueloliveirajob <samuel.santos.ext@dock.tech>
2020-12-01 17:52:56 -0300

    first

---
+++ src/main/java/com/kuberdocker/springbootkuber/SpringbootKuberApplication.java
@@ -0,0 +1,13 @@
+package com.kuberdocker.springbootkuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKuberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuberdocker/springbootkuber/SpringbootKuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuberdocker.springbootkuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
